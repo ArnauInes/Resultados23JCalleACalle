25e7d8e5b8e72c225538b7b2b14b6f0bde6488b1
Arnau Inés <suarsen@MacBook-Air-de-Marc.local>
2024-07-14 12:42:35 +0200

    Actualizar build.js para buscar archivos en el directorio public

--- build.js
+++ build.js
@@ -5,14 +5,14 @@ const dotenv = require('dotenv');
 // Cargar variables de entorno desde Vercel
 dotenv.config();
 
-// Leer el contenido de script.js
-let script = fs.readFileSync('./script.js', 'utf8');
+// Leer el contenido de script.js en el directorio public
+let script = fs.readFileSync('./public/script.js', 'utf8');
 
 // Reemplazar las variables de entorno
 script = script.replace('process.env.MAPBOX_ACCESS_TOKEN', `"${process.env.MAPBOX_ACCESS_TOKEN}"`);
 script = script.replace('process.env.MAPBOX_STYLE_URL', `"${process.env.MAPBOX_STYLE_URL}"`);
 
-// Escribir el nuevo contenido en script.js
-fs.writeFileSync('./script.js', script, 'utf8');
+// Escribir el nuevo contenido en script.js en el directorio public
+fs.writeFileSync('./public/script.js', script, 'utf8');
 
 console.log('Variables de entorno inyectadas en script.js');
