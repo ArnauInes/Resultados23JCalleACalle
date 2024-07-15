3363e0f9004f31872f51c232ec4dbc20a1518f7e
Arnau Inés <suarsen@MacBook-Air-de-Marc.local>
2024-07-15 10:45:28 +0200

    Configura el script como módulo y oculta las variables sensibles

--- public/config.js
+++ public/config.js
@@ -1,5 +1,3 @@
 // config.js
-require('dotenv').config();
-
-export const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
-export const MAPBOX_STYLE_URL = process.env.MAPBOX_STYLE_URL;
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXJuYXVpbmVzIiwiYSI6ImNsdnFjY3hiMjBkbDQybG1oaDl1MmZxN3IifQ.w9MXQRTssGGK-k7y6OZQSQ';
+export const MAPBOX_STYLE_URL = 'mapbox://styles/arnauines/clxn69hrc00id01r2c62dfyhr';
